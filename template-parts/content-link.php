c78ddaa7059c57e097147955146199f43bb60768
Bishal Napit <mebishalnapit@gmail.com>
2016-06-22 20:47:55 +0545

    CSS styling fixes for post metas

--- template-parts/content-link.php
+++ template-parts/content-link.php
@@ -9,76 +9,78 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php do_action('the_newsmag_before_post_content'); ?>
-    
-    <div class="link-details">
-        <a href="<?php echo the_newsmag_link_post_format(); ?>" target="_blank"><?php echo esc_html__('Link', 'the-newsmag'); ?></a>
-    </div>
+    <div class="article-container clear">
+        <?php do_action('the_newsmag_before_post_content'); ?>
 
-    <div class="post-header-wrapper clear">
-        <?php if ('post' === get_post_type()) : ?>
-            <div class="category-links">
-                <?php the_newsmag_colored_category(); ?>
-            </div><!-- .entry-meta -->
-        <?php endif; ?>
+        <div class="link-details">
+            <a href="<?php echo the_newsmag_link_post_format(); ?>" target="_blank"><?php echo esc_html__('Link', 'the-newsmag'); ?></a>
+        </div>
 
-        <?php if (('post' === get_post_type() && !is_single() && !post_password_required()) && (comments_open() || get_comments_number())) : ?>
-            <a href="<?php esc_url(comments_link()); ?>" class="entry-meta-comments">
-                <?php
-                printf(_nx('<i class="fa fa-comment"></i> 1', '<i class="fa fa-comment"></i> %1$s', get_comments_number(), 'comments title', 'the-newsmag'), number_format_i18n(get_comments_number()));
+        <div class="post-header-wrapper clear">
+            <?php if ('post' === get_post_type()) : ?>
+                <div class="category-links">
+                    <?php the_newsmag_colored_category(); ?>
+                </div><!-- .entry-meta -->
+            <?php endif; ?>
+
+            <?php if (('post' === get_post_type() && !is_single() && !post_password_required()) && (comments_open() || get_comments_number())) : ?>
+                <a href="<?php esc_url(comments_link()); ?>" class="entry-meta-comments">
+                    <?php
+                    printf(_nx('<i class="fa fa-comment"></i> 1', '<i class="fa fa-comment"></i> %1$s', get_comments_number(), 'comments title', 'the-newsmag'), number_format_i18n(get_comments_number()));
+                    ?>
+                </a>
+            <?php endif; ?>
+        </div>
+
+        <header class="entry-header">
+            <?php
+            if (is_single()) {
+                the_title('<h1 class="entry-title">', '</h1>');
+            } else {
+                the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+            }
+
+            if ('post' === get_post_type()) :
                 ?>
-            </a>
-        <?php endif; ?>
-    </div>
-
-    <header class="entry-header">
-        <?php
-        if (is_single()) {
-            the_title('<h1 class="entry-title">', '</h1>');
-        } else {
-            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-        }
-
-        if ('post' === get_post_type()) :
+                <div class="entry-meta">
+                    <?php the_newsmag_posted_on(); ?>
+                </div><!-- .entry-meta -->
+            <?php endif;
             ?>
-            <div class="entry-meta">
-                <?php the_newsmag_posted_on(); ?>
-            </div><!-- .entry-meta -->
-        <?php endif;
-        ?>
-    </header><!-- .entry-header -->
+        </header><!-- .entry-header -->
 
-    <div class="entry-content">
-        <?php
-        if (is_single()) :
-            the_content();
-        else :
-            the_excerpt(); // displaying excerpt for the archive pages
-        endif;
+        <div class="entry-content">
+            <?php
+            if (is_single()) :
+                the_content();
+            else :
+                the_excerpt(); // displaying excerpt for the archive pages
+            endif;
 
-        wp_link_pages(array(
-            'before' => '<div class="page-links">' . esc_html__('Pages:', 'the-newsmag'),
-            'after' => '</div>',
-        ));
-        ?>
+            wp_link_pages(array(
+                'before' => '<div class="page-links">' . esc_html__('Pages:', 'the-newsmag'),
+                'after' => '</div>',
+            ));
+            ?>
 
-        <?php if (!is_single() && !is_sticky()) : ?>
-            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                <?php
-                printf(
-                        /* translators: %s: Name of current post. */
-                        wp_kses('<button type="button" class="btn continue-more-link">' . __('Read More <i class="fa fa-arrow-circle-o-right"></i>', 'the-newsmag') . '</button> %s', array('i' => array('class' => array()), 'button' => array('class' => array(), 'type' => array()))), the_title('<span class="screen-reader-text">"', '"</span>', false)
-                );
-                ?>
-            </a>
+            <?php if (!is_single() && !is_sticky()) : ?>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php
+                    printf(
+                            /* translators: %s: Name of current post. */
+                            wp_kses('<button type="button" class="btn continue-more-link">' . __('Read More <i class="fa fa-arrow-circle-o-right"></i>', 'the-newsmag') . '</button> %s', array('i' => array('class' => array()), 'button' => array('class' => array(), 'type' => array()))), the_title('<span class="screen-reader-text">"', '"</span>', false)
+                    );
+                    ?>
+                </a>
+            <?php endif; ?>
+        </div><!-- .entry-content -->
+
+        <?php if (is_single()) : ?>
+            <footer class="entry-footer">
+                <?php the_newsmag_entry_footer(); ?>
+            </footer><!-- .entry-footer -->
         <?php endif; ?>
-    </div><!-- .entry-content -->
 
-    <?php if (is_single()) : ?>
-        <footer class="entry-footer">
-            <?php the_newsmag_entry_footer(); ?>
-        </footer><!-- .entry-footer -->
-    <?php endif; ?>
-
-    <?php do_action('the_newsmag_after_post_content'); ?>
+        <?php do_action('the_newsmag_after_post_content'); ?>
+    </div>
 </article><!-- #post-## -->
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,58 +9,60 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php do_action('the_newsmag_before_post_content'); ?>
+    <div class="article-container clear">
+        <?php do_action('the_newsmag_before_post_content'); ?>
 
-    <div class="post-header-wrapper clear">
-        <?php if (has_post_thumbnail()) : ?>
+        <div class="post-header-wrapper clear">
+            <?php if (has_post_thumbnail()) : ?>
 
+                <?php
+                $image_popup_id = get_post_thumbnail_id();
+                $image_popup_url = wp_get_attachment_url($image_popup_id);
+                ?>
+
+                <figure class="featured-image">
+                    <?php if (get_theme_mod('the_newsmag_featured_image_popup', 0) == 1) { ?>
+                        <a href="<?php echo $image_popup_url; ?>" class="featured-image-popup" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('the-newsmag-featured-large-thumbnail'); ?></a>
+                    <?php } else { ?>
+                        <?php the_post_thumbnail('the-newsmag-featured-large-thumbnail'); ?>
+                    <?php } ?>
+                </figure>
+
+            <?php endif; ?>
+        </div>
+
+        <header class="entry-header">
             <?php
-            $image_popup_id = get_post_thumbnail_id();
-            $image_popup_url = wp_get_attachment_url($image_popup_id);
+            if (is_front_page()) :
+                the_title('<h2 class="entry-title">', '</h2>');
+            else :
+                the_title('<h1 class="entry-title">', '</h1>');
+            endif;
             ?>
+        </header><!-- .entry-header -->
 
-            <figure class="featured-image">
-                <?php if (get_theme_mod('the_newsmag_featured_image_popup', 0) == 1) { ?>
-                    <a href="<?php echo $image_popup_url; ?>" class="featured-image-popup" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('the-newsmag-featured-large-thumbnail'); ?></a>
-                <?php } else { ?>
-                    <?php the_post_thumbnail('the-newsmag-featured-large-thumbnail'); ?>
-                <?php } ?>
-            </figure>
+        <div class="entry-content">
+            <?php
+            the_content();
 
-        <?php endif; ?>
+            wp_link_pages(array(
+                'before' => '<div class="page-links">' . esc_html__('Pages:', 'the-newsmag'),
+                'after' => '</div>',
+            ));
+            ?>
+        </div><!-- .entry-content -->
+
+        <footer class="entry-footer">
+            <?php
+            edit_post_link(
+                    wp_kses(sprintf(
+                                    /* translators: %s: Name of current post */
+                                    __('<i class="fa fa-edit"></i>Edit %s', 'the-newsmag'), the_title('<span class="screen-reader-text">"', '"</span>', false)
+                            ), array('i' => array('class' => array()), 'span' => array('class' => array()))), '<span class="edit-link">', '</span>'
+            );
+            ?>
+        </footer><!-- .entry-footer -->
+
+        <?php do_action('the_newsmag_after_post_content'); ?>
     </div>
-
-    <header class="entry-header">
-        <?php
-        if (is_front_page()) :
-            the_title('<h2 class="entry-title">', '</h2>');
-        else :
-            the_title('<h1 class="entry-title">', '</h1>');
-        endif;
-        ?>
-    </header><!-- .entry-header -->
-
-    <div class="entry-content">
-        <?php
-        the_content();
-
-        wp_link_pages(array(
-            'before' => '<div class="page-links">' . esc_html__('Pages:', 'the-newsmag'),
-            'after' => '</div>',
-        ));
-        ?>
-    </div><!-- .entry-content -->
-
-    <footer class="entry-footer">
-        <?php
-        edit_post_link(
-                wp_kses(sprintf(
-                                /* translators: %s: Name of current post */
-                                __('<i class="fa fa-edit"></i>Edit %s', 'the-newsmag'), the_title('<span class="screen-reader-text">"', '"</span>', false)
-                        ), array('i' => array('class' => array()), 'span' => array('class' => array()))), '<span class="edit-link">', '</span>'
-        );
-        ?>
-    </footer><!-- .entry-footer -->
-
-    <?php do_action('the_newsmag_after_post_content'); ?>
 </article><!-- #post-## -->
