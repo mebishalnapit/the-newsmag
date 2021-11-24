47b377fef5324a11adb8754d33a90df3dc21e9bc
Bishal Napit <mebishalnapit@gmail.com>
2021-11-24 10:56:37 +0545

    Fix searchform.php file escapings

--- searchform.php
+++ searchform.php
@@ -4,11 +4,12 @@
  *
  * @package The NewsMag
  */
+
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo esc_html_e('Search for:', 'the-newsmag') ?></span>
-		<input type="search" class="search-field" placeholder="<?php esc_attr_e('Search for&hellip;', 'the-newsmag') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php esc_attr_e('Search for:', 'the-newsmag') ?>" />
+		<span class="screen-reader-text"><?php esc_html_e( 'Search for:', 'the-newsmag' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search for&hellip;', 'the-newsmag' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php esc_attr_e( 'Search for:', 'the-newsmag' ); ?>" />
 	</label>
 	<button class="searchsubmit" name="submit" type="submit"><i class="fa fa-search"></i></button>
 </form>
